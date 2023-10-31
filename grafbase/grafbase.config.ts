6b34a9bb144912d1b7ca705607e469b8af549f8c
genagurbanguliyev <genagurbanguliyev@gmail..com>
2023-10-31 14:45:20 +0500

    err

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
-import { g, auth, config } from "@grafbase/sdk";
-
-const User = g.model("User", {
-  name: g.string().length({ min: 2, max: 20 }),
-  email: g.string().unique(),
-  avatarUrl: g.url(),
-  description: g.string().optional(),
-  githubUrl: g.url().optional(),
-  linkedInUrl: g.url().optional(),
-  projects: g
-    .relation(() => Projects)
-=======
-import { g, config, auth } from "@grafbase/sdk";
+import { g, config } from "@grafbase/sdk";
 
 // @ts-ignore
 const User = g.model("User", {
@@ -23,41 +10,21 @@ const User = g.model("User", {
   linkedinUrl: g.url().optional(),
   projects: g
     .relation(() => Project)
->>>>>>> adce5e0 (graf_config)
     .list()
     .optional(),
 });
 
-<<<<<<< HEAD
-const Projects = g.model("Projects", {
-=======
 // @ts-ignore
 const Project = g.model("Project", {
->>>>>>> adce5e0 (graf_config)
   title: g.string().length({ min: 3 }),
   description: g.string(),
   image: g.url(),
   liveSiteUrl: g.url(),
-<<<<<<< HEAD
-  hithubUrl: g.url(),
-=======
   githubUrl: g.url(),
->>>>>>> adce5e0 (graf_config)
   category: g.string().search(),
   createdBy: g.relation(() => User),
 });
 
 export default config({
   schema: g,
-<<<<<<< HEAD
-  // Integrate Auth
-  // https://grafbase.com/docs/auth
-  // auth: {
-  //   providers: [authProvider],
-  //   rules: (rules) => {
-  //     rules.private()
-  //   }
-  // }
-=======
->>>>>>> adce5e0 (graf_config)
 });
